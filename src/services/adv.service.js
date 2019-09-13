b6963d791c9ea73df32d1d287338735c31359842
Giuppi <g.funicello@gmail.com>
2019-09-13 15:40:16 +0200

    remove morgan body

--- src/services/adv.service.js
+++ src/services/adv.service.js
@@ -17,11 +17,11 @@ const advService = (options) => {
           .fontSize(25)
           .text(product.name, 100, 100)
 
-        /*doc.image(path.join(storagePath,product.image), {
+        doc.image(path.join('https://media.farmchain.it',product.image), {
             fit: [250, 300],
             align: 'left',
             valign: 'top'
-         });*/
+         });
          doc.end();
 
         //configuring the AWS environment
